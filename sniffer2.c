825832054e64ebd6632ce198244106ce474998c8
epa480 <epa480@gmail.com>
2019-11-07 14:33:23 -0300

    Create sniffer2.c

---
+++ sniffer2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <linux/if_ether.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+
+int main(int argc, char **argv)
+{
+    int sock, n;
+    char buffer[2048];
+    unsigned char *iphead, *ethhead;
+    struct ifreq ethreq;
+
+    if ((sock = socket(PF_PACKET, SOCK_RAW,
+                       htons(ETH_P_IP))) < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    strncpy(ethreq.ifr_name, "enp2s0", IFNAMSIZ);
+    if (ioctl(sock, SIOCGIFFLAGS, &ethreq) == -1)
+    {
+        perror("ioctl");
+        close(sock);
+        exit(1);
+    }
+    ethreq.ifr_flags |= IFF_PROMISC;
+    if (ioctl(sock, SIOCSIFFLAGS, &ethreq) == -1)
+    {
+        perror("ioctl");
+        close(sock);
+        exit(1);
+    }
+
+    while (1)
+    {
+        printf("----------\n");
+        n = recvfrom(sock, buffer, 2048, 0, NULL, NULL);
+        printf("%d bytes read\n", n);
+
+        if (n < 42)
+        {
+            perror("recvfrom():");
+            printf("Incomplete packet (errno is %d)\n",
+                   errno);
+            close(sock);
+            exit(0);
+        }
+
+        ethhead = buffer;
+        printf("Source MAC address: "
+               "%02x:%02x:%02x:%02x:%02x:%02x\n",
+               ethhead[0], ethhead[1], ethhead[2],
+               ethhead[3], ethhead[4], ethhead[5]);
+        printf("Destination MAC address: "
+               "%02x:%02x:%02x:%02x:%02x:%02x\n",
+               ethhead[6], ethhead[7], ethhead[8],
+               ethhead[9], ethhead[10], ethhead[11]);
+
+        iphead = buffer + 14; 
+        if (*iphead == 0x45)
+        {
+            printf("Source host %d.%d.%d.%d\n",
+                   iphead[12], iphead[13],
+                   iphead[14], iphead[15]);
+            printf("Dest host %d.%d.%d.%d\n",
+                   iphead[16], iphead[17],
+                   iphead[18], iphead[19]);
+            printf("Source,Dest ports %d,%d\n",
+                   (iphead[20] << 8) + iphead[21],
+                   (iphead[22] << 8) + iphead[23]);
+            printf("Layer-4 protocol %d\n", iphead[9]);
+        }
+    }
+}
